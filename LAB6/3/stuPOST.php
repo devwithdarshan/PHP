798f48409a4d15089bf25b042410a3e1f9793343
Darshan Lila <darshan1315du@gmail.com>
2025-01-22 18:49:56 +0530

    Add files via upload

---
+++ LAB6/3/stuPOST.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Student Registration</title>
+</head>
+<body>
+	<form action="Con_stuPOST.php" method="POST">
+		<label> Name:- </label>
+	    <input type ='text' name="name"><br>
+	    <label> Email:-</label>
+	    <input type ='email' name="email"><br>
+	    <label> Password:- </label>
+	    <input type ='password' name="password"><br> 
+	    <input type ='submit' value='Register' name="submit">   
+    </form>
+</body>
+</html>
